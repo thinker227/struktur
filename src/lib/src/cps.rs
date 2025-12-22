7befcfbd9269e1d56d4e99024cc9a41a07a75d35
Thinker <thinker0227@gmail.com>
2025-12-22 01:17:27 +0100

    An attempt

--- src/lib/src/cps.rs
+++ src/lib/src/cps.rs
@@ -49,8 +49,9 @@ pub struct GenSymbol(Id);
 /// A complex expression which always terminates in a function call instead of producing a value.
 #[derive(Debug, Clone)]
 pub enum Complex {
-    /// A call to a function produced by an atomic expression, with some value as the argument.
-    Call(Target, Atomic),
+    /// A call to a function produced by an atomic expression, with some value as the argument
+    /// and an optional argument to pass as the continuation for the function, if it takes one.
+    Call(Target, Atomic, Option<Atomic>),
     /// Introduces a let-binding with a nested complex expression.
     Let(Box<Let>),
     /// Branches to two complex sub-expressions.
@@ -60,8 +61,6 @@ pub enum Complex {
 /// The target of a function call.
 #[derive(Debug, Clone)]
 pub enum Target {
-    /// Call a continuation.
-    Cont(Continuation),
     /// Call a function producted by an atomic expression.
     Expr(Atomic),
     /// Halt execution.
@@ -100,6 +99,62 @@ pub enum Atomic {
     Var(CpsSymbol),
     Cont(Continuation),
     Lambda(Box<Lambda>),
+}
+
+static CONTINUATION_PROVIDER: IdProvider<Continuation> = IdProvider::new(Continuation);
+
+static GEN_PROVIDER: IdProvider<GenSymbol> = IdProvider::new(GenSymbol);
+
+fn m(e@ast::Expr(expr, _, _): &ast::Expr<Typed>) -> Atomic {
+    match expr {
+        ast::ExprVal::Unit => Atomic::Unit,
+        ast::ExprVal::Int(x) => Atomic::Int(*x),
+        ast::ExprVal::Bool(x) => Atomic::Bool(*x),
+        ast::ExprVal::String(s) => Atomic::String(s.clone()),
+        ast::ExprVal::Var(var) => Atomic::Var(CpsSymbol::Symbol(*var)),
+        ast::ExprVal::Bind(_) => unimplemented!(),
+        ast::ExprVal::Lambda(lambda) => {
+            let cont = CONTINUATION_PROVIDER.next();
+            Atomic::Lambda(Box::new(Lambda {
+                param: CpsSymbol::Symbol(lambda.param),
+                cont: Some(cont),
+                body: t(&lambda.body, Atomic::Cont(cont))
+            }))
+        }
+        ast::ExprVal::Apply(_) => unimplemented!(),
+        ast::ExprVal::If(_) => unimplemented!()
+    }
+}
+
+fn t(e@ast::Expr(expr, _, _): &ast::Expr<Typed>, cont: Atomic) -> Complex {
+    match expr {
+        ast::ExprVal::Bind(binding) => todo!(),
+        ast::ExprVal::Apply(application) => {
+            let target = CpsSymbol::Gen(GEN_PROVIDER.next());
+            let arg = CpsSymbol::Gen(GEN_PROVIDER.next());
+            t(
+                &application.target,
+                Atomic::Lambda(Box::new(Lambda {
+                    param: target,
+                    cont: None,
+                    body: t(
+                        &application.arg,
+                        Atomic::Lambda(Box::new(Lambda {
+                            param: arg,
+                            cont: None,
+                            body: Complex::Call(
+                                Target::Expr(Atomic::Var(target)),
+                                Atomic::Var(arg),
+                                Some(cont)
+                            )
+                        }))
+                    )
+                }))
+            )
+        }
+        ast::ExprVal::If(if_else) => todo!(),
+        _ => Complex::Call(Target::Expr(cont), m(e), None)
+    }
 }
 
 /// Contains all info returned by transforming an AST into CPS.
