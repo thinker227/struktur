92a0459c4641aeaea1f721185e8f8e56758ae59b
Thinker <thinker0227@gmail.com>
2026-05-23 20:50:35 +0200

    Update some type stuff

--- src/compiler/src/types.rs
+++ src/compiler/src/types.rs
@@ -1,9 +1,8 @@
 //! Definitions of Struktur types.
 
-use std::rc::Rc;
+use crate::{symbols::Symbol, syntax::NodeId};
 
-use crate::{symbols::Symbol, text::TextLocation};
-
+pub use check::type_check;
 pub use meta_var::MetaVar;
 
 mod check;
@@ -14,7 +13,7 @@ mod meta_var;
 /// Only top-level bindings have a [PolyType] as their type.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum PolyType {
-    Forall(ForallType),
+    Forall(Ty<ForallType>),
     Type(MonoType),
 }
 
@@ -23,9 +22,14 @@ pub enum PolyType {
 /// This defines the type of expressions and variables.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum MonoType {
+    /// A primitive.
     Primitive(Ty<PrimitiveType>),
-    Function(Rc<Ty<FunctionType>>),
+    /// A function.
+    Function(Ty<Box<FunctionType>>),
+    /// A type variable.
     Var(Ty<TypeVar>),
+    /// A unification variable.
+    /// This variant should *only* be present during unification.
     Meta(MetaVar),
 }
 
@@ -42,9 +46,9 @@ pub enum PrimitiveType {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct FunctionType {
     /// The function's parameter/input type.
-    pub param: Ty<MonoType>,
+    pub param: MonoType,
     /// The function's return/output type.
-    pub ret: Ty<MonoType>,
+    pub ret: MonoType,
 }
 
 /// A type quantified/generalized over a set of type variables.
@@ -55,7 +59,7 @@ pub struct ForallType {
     /// The type variables being quantified.
     pub vars: Vec<TypeVar>,
     /// The inner type being generalized over.
-    pub generalized: Ty<MonoType>,
+    pub generalized: MonoType,
 }
 
 /// A type variable introduced by a [ForallType].
@@ -63,12 +67,48 @@ pub struct ForallType {
 /// Not to be confused with [MetaVar]. [TypeVar] is a variable in the finalized typing of the program,
 /// while [MetaVar] is a variable which only exists during type inference and which can be freely
 /// substituted for other types.
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct TypeVar {
     /// The type variable *symbol* the type is the *type* version of.
     ///
     /// I.e. this [TypeVar] is the *type* version of this type variable *[Symbol]*.
     pub symbol: Symbol,
+}
+
+impl From<Ty<PrimitiveType>> for MonoType {
+    fn from(value: Ty<PrimitiveType>) -> Self {
+        Self::Primitive(value)
+    }
+}
+
+impl From<Ty<Box<FunctionType>>> for MonoType {
+    fn from(value: Ty<Box<FunctionType>>) -> Self {
+        Self::Function(value)
+    }
+}
+
+impl From<Ty<TypeVar>> for MonoType {
+    fn from(value: Ty<TypeVar>) -> Self {
+        Self::Var(value)
+    }
+}
+
+impl From<MetaVar> for MonoType {
+    fn from(value: MetaVar) -> Self {
+        Self::Meta(value)
+    }
+}
+
+impl From<MonoType> for PolyType {
+    fn from(value: MonoType) -> Self {
+        Self::Type(value)
+    }
+}
+
+impl From<Ty<ForallType>> for PolyType {
+    fn from(value: Ty<ForallType>) -> Self {
+        Self::Forall(value)
+    }
 }
 
 /// A type with extra data attached.
@@ -78,6 +118,23 @@ pub struct Ty<T> {
     pub ty: T,
     /// The type's provenance.
     pub provenance: Provenance,
+}
+
+impl<T> Ty<T> {
+    /// Maps the type.
+    pub fn map<U>(self, f: impl FnOnce(T) -> U) -> Ty<U> {
+        Ty {
+            ty: f(self.ty),
+            provenance: self.provenance,
+        }
+    }
+
+    pub fn with<U>(&self, ty: U) -> Ty<U> {
+        Ty {
+            ty,
+            provenance: self.provenance.clone(),
+        }
+    }
 }
 
 /// The provenance of a type.
@@ -91,7 +148,7 @@ pub struct Ty<T> {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Provenance {
     /// A specific point in the type flow of the program.
-    Location(TextLocation),
+    Location(NodeId),
     /// A path through several provenances.
     Path(Vec<Provenance>),
     /// The parameter type of a function.
