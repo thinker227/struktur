31ff80baf604001127367c519b03c568768af636
Thinker <thinker0227@gmail.com>
2026-05-27 00:18:47 +0200

    Add select_nodes macro

--- src/compiler/src/syntax/tree.rs
+++ src/compiler/src/syntax/tree.rs
@@ -643,6 +643,95 @@ macro_rules! make_node {
     };
 }
 
+/// CSS-like selector syntax for syntax trees.
+/// Returns an iterator of descendant nodes which match the selector.
+///
+/// The selector is made up of one or more *clauses*:
+/// - `> *`: Selects all immediate child nodes.
+///   - `> *[n]`: Selects the `n`th immediate child node.
+/// - `> (k)`: Selects all immediate child nodes with kind `k`.
+///   - `> (k)[n]`: Selects the `n`th child node with kind `k`.
+/// - `(k)`: Selects all descendant nodes with kind `k`.
+///
+/// # Examples
+/// ```ignore
+/// // select all immediate children
+/// select_nodes!(node => > *);
+///
+/// // select all `LitExpr` nodes which are immediate children of an `Expr` node
+/// select_nodes!(node => (Kind::Expr) > (Kind::LitExpr))
+///
+/// // select all children of the first `Stmt` node
+/// select_nodes!(node => > (Kind::Stmt)[0] > *)
+///
+/// // select the first child of every immediate child
+/// select_nodes!(node => > *[0])
+/// ```
+/// Due to the syntax limitations of declarative macros, kind clauses have to be enclosed in parentheses.
+#[macro_export]
+macro_rules! select_nodes {
+    // Entry arm (nicer surface syntax)
+    ($tree:expr => $($selector:tt)*) => {
+        select_nodes!(
+            std::iter::once($tree);
+            $($selector)*
+        )
+    };
+
+    // Immediate child indexing
+    ($tree:expr; > * [$index:expr] $($rest:tt)*) => {
+        select_nodes!(
+            $tree.filter_map(|x| x.node($index));
+            $($rest)*
+        )
+    };
+
+    // Immediate children
+    ($tree:expr; > * $($rest:tt)*) => {
+        select_nodes!(
+            $tree.flat_map(|x| x.nodes());
+            $($rest)*
+        )
+    };
+
+    // Immediate child indexing with filter
+    ($tree:expr; > ($kind:expr) [$index:expr] $($rest:tt)*) => {
+        select_nodes!(
+            $tree.filter_map(|x| x
+                .nodes()
+                .filter(|x| x.kind() == &$kind)
+                .nth($index)
+            );
+            $($rest)*
+        )
+    };
+
+    // Immediate children with filter
+    ($tree:expr; > ($kind:expr) $($rest:tt)*) => {
+        select_nodes!(
+            $tree
+                .flat_map(|x| x.nodes())
+                .filter(|x| x.kind() == &$kind);
+            $($rest)*
+        )
+    };
+
+    // Descendants with filter
+    ($tree:expr; ($kind:expr) $($rest:tt)*) => {
+        select_nodes!(
+            $tree
+                .flat_map(|x| x.all_descendants())
+                .filter(|x| x.kind() == &$kind);
+            $($rest)*
+        )
+    };
+
+    // Exit arm
+    ($tree:expr;) => {
+        $tree
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
